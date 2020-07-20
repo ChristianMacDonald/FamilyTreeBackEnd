deefaf79a2e1225088afc7ae27c028eaa6228820
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2020-07-19 20:20:50 -0400

    Fix capitalization of PORT environment variable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 const server = require('./api/server');
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`API running on port ${port}`));
