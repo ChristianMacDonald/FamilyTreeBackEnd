062160322264d007d90a166217295a6f08247296
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2020-12-05 06:41:30 -0500

    Enable CORS requests

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const server = express();
 const apiRouter = require('./apiRouter');
 
 server.use(express.json());
+server.use(cors());
 
 server.use('/api', apiRouter);
 
