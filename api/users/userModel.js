bcce8ab49b1bee1b10f23fb8a7cfb76ba4a7eea8
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2021-01-24 13:35:43 -0500

    Remove password hash from response to GET request to api/users/:username endpoint

--- api/users/userModel.js
+++ api/users/userModel.js
@@ -9,7 +9,7 @@ function findById(id) {
 }
 
 function findByUsername(username) {
-    return db('users').where({ username }).first();
+    return db('users').where({ username }).first('id', 'username');
 }
 
 async function add(user) {
